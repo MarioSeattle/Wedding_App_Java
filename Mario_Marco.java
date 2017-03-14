b3a1979ce96f47eb1e5c5707f12b947791064628
MarioSeattle <marioseattle@live.com>
2017-03-14 16:50:05 -0700

    Final version assigment 6

--- Mario_Marco.java
+++ Mario_Marco.java
@@ -13,18 +13,17 @@
  *Description: This program will allow the user to input an amount of guests
  *and tables, and will be able to output each guest person's name in the output window. 
  */
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner; // import the scanner to be used
+import java.io.*;
+import java.util.*; // import the scanner to be used
 import myClasses.Person;
 
 		public class Mario_Marco {
 			
-			public static void main(String[] args) throws FileNotFoundException {
+			public static void main(String[] args) throws IOException {
 				
 				int numGuests, TableSize;
 				
-				Scanner inFile = new Scanner(new File("/Users/Mgomez/documents/workspace/Mario_Marco/src/500names.txt"));
+				Scanner inFile = new Scanner(new File("\Users\Valued Customer\Documents\GitHub\Wedding_App_Java\500names.txt"));
 				
 				int guestId = 0;
 				
@@ -48,35 +47,62 @@ import myClasses.Person;
 					guestId++ ;
 				}
 				
-			for (int i = 0; i < guestId; i++) {
-						
-					System.out.print(guests[i] + "\n");
-						
-					}
+			
+			
+			//The date of the wedding
+			
+			java.util.Date date = new java.util.Date();
+		    System.out.println(date);
 					
 			
-					System.out.println("Please enter number of guests attending the wedding: \n");
+			System.out.println("Please enter number of guests attending the wedding: \n");
 					
-					Scanner scanner = new Scanner(System.in); //take input
+			Scanner scanner = new Scanner(System.in); //take input
 					
-						numGuests = scanner.nextInt(); // Gathers user data for size of wedding.
+			numGuests = scanner.nextInt(); // Gathers user data for size of wedding.
 					 
-					System.out.println("Please enter the desired number of people at each table: \n ");
+			System.out.println("Please enter the desired number of people at each table: \n ");
 					
-					Scanner scanner1 = new Scanner(System.in); //take input
+					
 						
-						TableSize = scanner.nextInt(); // Gathers user data for size of each table
-						
-				
-					System.out.print(guestId + "\n");
+			TableSize = scanner.nextInt(); // Gathers user data for size of each table
+					
+					
 					
 					scanner.close();
-					scanner1.close();
-					inFile.close();
 					
-					}// end main
+					inFile.close();
 				
+					
+			int numTables = numGuests / TableSize;
+			if (numGuests % TableSize != 0) {
+				numTables++;
+			}
+			Table[] tables = new Table[numTables]; // made spot to save table
 			
+			int tableId = -1; // tableId is the array of tables created
+			
+			for ( int i = 0; i < numGuests; i++) { // i is equal to guest index
+				if ( i % TableSize == 0) {
+					tableId++; // if the table is full, increment tables by one
+					tables[tableId] = new Table(TableSize, tableId);
 				}
+				tables[tableId].addGuest(guests[i]);
+			}
 			
+			Writer out = new FileWriter("src/SeatingPlan.txt");
+			for (int i = 0; i < tables.length; i++) {
+				out.write(tables[i].toString());
+			}
+			
+			out.flush();
+			out.close();
+		
+	        }// end main
+				   
+
+	  }  // end class
+	       
+	        
+
 		
